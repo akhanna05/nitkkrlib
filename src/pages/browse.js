07bc368533215c59095be6429a39c586bc8ab3f7
ankit <ankitkhanna0506@gmail.com>
2021-06-09 14:31:36 +0530

    added backend

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
 import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-  const { lectures } = useContent("lectures");
-  // const { practicals } = useContent("practicals");
-console.log(lectures)
-  const slides = selectionFilter({ lectures, practicals: lectures });
+  const [lectures, setLectures] = useState([])
+  const [practicals, setPracticals] = useState([])
+   fetch('https://e-classes-c4a7a-default-rtdb.firebaseio.com/practicals.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const practicals = Object.values(data)[0].lectures;
+    setPracticals(practicals)
+  });
+  fetch('https://e-classes-c4a7a-default-rtdb.firebaseio.com/lectures.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const lectures = Object.values(data)[0];
+   setLectures(lectures)
+  });
+  
+  const slides = selectionFilter({ lectures, practicals });
 
   return <BrowseContainer slides={slides} />;
 }
